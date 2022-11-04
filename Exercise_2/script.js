0a90f3bee96079088f956f48d2306123d94f8c33
Nikhil-Singla <nsingla3.14@gmail.com>
2022-11-04 05:07:49 -0500

    Let functions

--- Exercise_2/script.js
+++ Exercise_2/script.js
@@ -1,11 +1,11 @@
-var images = ["https://miro.medium.com/max/500/1*ZhYNqU2y96_f3QkWq9oiWQ.jpeg", 
+let images = ["https://miro.medium.com/max/500/1*ZhYNqU2y96_f3QkWq9oiWQ.jpeg", 
 "https://www.kidscodecs.com/wp-content/uploads/2020/02/History_TS_ProgrammingMemes_image6.png",
 "https://images3.memedroid.com/images/UPLOADED478/6163397ee2cb3.jpeg",
 "https://imgix.ranker.com/user_node_img/50106/1002119125/original/1002119125-photo-u1?auto=format&q=60&fit=crop&fm=pjpg&dpr=2&w=375",
 "https://www.kidscodecs.com/wp-content/uploads/2020/02/History_TS_ProgrammingMemes_image2.png"
 ]
 
-var start = 0
+let start = 0
 var image = document.getElementById("img");
 
 function next()
