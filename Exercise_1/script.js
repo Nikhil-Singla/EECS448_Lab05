937e3919e132e03241fb29cb231d9f32471a1148
Nikhil-Singla <nsingla3.14@gmail.com>
2022-11-09 23:21:47 -0600

    Making button toggleable

--- Exercise_1/script.js
+++ Exercise_1/script.js
@@ -4,15 +4,32 @@ function Validate()
     var secondPass = document.getElementById("second").value; 
     if(initialPass != secondPass)
     {
-        alert("Passwords Don't Match!")
+        alert("Passwords Don't Match!");
     }
     else if(initialPass.length < 8) 
     {
-        alert("Passwords Length less than 8!")
+        alert("Passwords Length less than 8!");
     }
     else
     {
-        alert("Passwords Match!")
+        alert("Passwords Match!");
+    }
+
+}
+
+function changeType()
+{
+    let typeOne = document.getElementById("first"); 
+    let typeTwo = document.getElementById("second"); 
+    if(typeOne.type == 'text')
+    {
+        typeOne.type = 'password';
+        typeTwo.type = 'password';
+    }
+    else
+    {
+        typeOne.type = 'text';
+        typeTwo.type = 'text';
     }
 
 }
